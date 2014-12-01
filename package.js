fe6aad21be776e611bc811ed5b6cb1877aa88b16
Mike Kornelson <mike@koretech.ca>
2014-12-01 10:29:41 -0600

    Upgraded to Meteor 1.0

--- package.js
+++ package.js
@@ -3,13 +3,13 @@ var client = 'client', server = 'server', both = ['client', 'server'];
 Package.describe({
 	name: 'krt:dropkick',
 	summary: 'Dropkick Package',
-	version: '2.0.1_1',
+	version: '2.0.1_2',
 	git: 'https://github.com/koretech/meteor-krt-dropkick.git'
 });
 
 Package.onUse(function(api){
 
-	api.versionsFrom('METEOR@0.9.4');
+	api.versionsFrom('METEOR@1.0');
 
 	api.addFiles([
 		'lib/dropkick/dropkick.js',
